847fe6f63557a3ba707a443c4b813bd3943fe95b
djhocking <djhocking@gmail.com>
2018-10-25 17:19:56 -0400

    updated code together for one site

--- Code/TurtleSCR.R
+++ Code/TurtleSCR.R
@@ -1,43 +1,50 @@
 
 ##### SCR Analysis Data Compilation and Model Creation Using BUGS and JAGS ####
 
-library(AHMbook)
-library(R2jags)  #rjags could not be loaded?
+library(dplyr)
+library(tidyr)
+# library(AHMbook)
+# library(R2jags)  #rjags could not be loaded?
 library(jagsUI)
-library(R2WinBUGS)
+# library(R2WinBUGS)
 
-EDF <- read.csv(file = "~/Desktop/Thesis/cocanal-turtles/Data/EDF.csv")
+EDF <- read.csv(file = "Data/EDF.csv", stringsAsFactors = FALSE)
 head(EDF)
+
 #Add a new column for integer session values (session = site)
-EDF$session <- NA
-head(EDF)
-EDF$session <- ifelse(EDF$site == "A", 1,
-                  ifelse(EDF$site == "C", 2,
-                    ifelse(EDF$site == "D", 3,
-                      ifelse(EDF$site == "E", 4,
-                         ifelse(EDF$site == "F", 5,
-                           ifelse(EDF$site == "G", 6,
-                             ifelse(EDF$site == "H", 7,
-                               ifelse(EDF$site == "I", 8,
-                                 ifelse(EDF$site == "J", 9,
-                                   ifelse(EDF$site == "K", 10,
-                                     ifelse(EDF$site == "L", 11,
-                                       ifelse(EDF$site == "M", 12,
-                                        ifelse(EDF$site == "N", 13,
-                                          ifelse(EDF$site == "O", 14, NA)
-                                        )))))))))))))
-head(EDF)
+
+EDF$site_num <- as.integer(as.factor(EDF$site))
+# 
+# EDF$session <- NA
+# head(EDF)
+# EDF$session <- ifelse(EDF$site == "A", 1,
+#                   ifelse(EDF$site == "C", 2,
+#                     ifelse(EDF$site == "D", 3,
+#                       ifelse(EDF$site == "E", 4,
+#                          ifelse(EDF$site == "F", 5,
+#                            ifelse(EDF$site == "G", 6,
+#                              ifelse(EDF$site == "H", 7,
+#                                ifelse(EDF$site == "I", 8,
+#                                  ifelse(EDF$site == "J", 9,
+#                                    ifelse(EDF$site == "K", 10,
+#                                      ifelse(EDF$site == "L", 11,
+#                                        ifelse(EDF$site == "M", 12,
+#                                         ifelse(EDF$site == "N", 13,
+#                                           ifelse(EDF$site == "O", 14, NA)
+#                                         )))))))))))))
+# head(EDF)
 summary(EDF)
 
 ## Creating model for 1 site first
-EDFA <- subset(EDF, session == "1")
+EDFA <- EDF %>%
+  filter(site_num == 1 & species == "CPIC")
 EDFA
 
 # Create a Trap Location Matrix (integers = distance apart in m)
-traplocsA <- cbind(0,25,50,75,100,125,150,175) # create trap location file
+traplocsA <- c(0,25,50,75,100,125,150,175) # create trap location file
 #this is in a vertical format
 traplocsA
-matrixA <- matrix(NA, ncol = ncol(traplocsA), nrow = ncol(traplocsA))
+matrixA <- matrix(NA, ncol = length(traplocsA), nrow = length(traplocsA))
 matrixA
 matrixA[ ,1] <- c(0,25,50,75,100,125,150,175)
 matrixA[ ,2] <- c(25,0,25,50,75,100,125,150)
@@ -49,40 +56,50 @@ matrixA[ ,7] <- c(150,125,100,75, 50,25,0,25)
 matrixA[ ,8] <- c(175,150,125,100,75, 50,25,0)
 matrixA  # will need to use coordinates if use all sites in 1 model! or figure out distance b/w sites
 
-ntrapsA <- 8 # number of traps
-as.character(EDFA$recap)
-N <- 44
-K <- 4 # trap nights per session
-buffer <- 50 # check literature to make sure doesn't need to be larger
+n_traps <- ncol(matrixA) # number of traps
+# as.character(EDFA$recap)
+N <- nrow(EDFA[which(EDFA$recap == "N"), ])
+K <- max(EDFA$day) # trap nights per session
+buffer <- 100 # check literature to make sure doesn't need to be larger
 #xlimA <- c(min(traplocsA[1,] - buffer), max(traplocs[1,] + buffer))
-xlimA <- c(-50, 225)
+xlimA <- c(min(matrixA)-buffer, max(matrixA) + buffer)
 xlimA
-EM <- read.csv(file = "~/Desktop/Thesis/cocanal-turtles/Data/EncounterMatrixA.csv")
-head(EM)
-EM[is.na(EM)] <- 0
-EM <- as.matrix(EM)
-EM <- cbind(1:42, rowSums(EM))
+n_ind <- length(unique(EDFA$ind))
+
+# Make encounter histories with number of times each individual is captured in each trap
+str(EDFA)
+
+EM <- EDFA %>%
+  group_by(ind, trap) %>%
+  select(trap, ind) %>%
+  mutate(count = 1) %>%
+  summarise_all(sum) %>%
+  spread(trap, count, fill = 0) %>%
+  ungroup()
+  
+EM <- as.matrix(data.frame(select(EM, -ind)))
+# EM <- cbind(1:n_ind, rowSums(EM))
+# EM <- rowSums(EM)
 # Read in trap hour file
-traphoursA <- read.csv(file = "~/Desktop/Thesis/cocanal-turtles/Data/traphoursA.csv")
+traphoursA <- read.csv(file = "Data/traphoursA.csv", stringsAsFactors = FALSE)
 head(traphoursA)
-K <- apply(traphoursA[ , 2:ncol(traphoursA)], 1, sum)
-K
+hours <- apply(traphoursA[ , 2:ncol(traphoursA)], 1, sum)
 
 
 # Data Augmentation
 M <- 200
-nind <- nrow(EM)
-J <- 8
-y <- rbind(EM, matrix(0, nrow = M-nind, ncol=ncol(EM)))
-z <- c(rep(1, nind), rep(0, M-nind))
+J <- n_traps
+# y <- rbind(EM[ , 2], matrix(0, nrow = M-n_ind, ncol = n_ind))
+y <- rbind(EM, matrix(0, nrow = M - n_ind, ncol = n_traps))
+z <- c(rep(1, n_ind), rep(0, M-n_ind))
 
 
 #Start values for s (activity centers) of augments (from random uniform constrained by state space size)
 X <- traplocsA
-sst <- cbind(runif(M, xlimA[1], xlimA[2])) #parameters, n, max, min
+sst <- c(runif(M, xlimA[1], xlimA[2])) #parameters, n, max, min
 sst # Now populated by starting positions uniformally placed within state space
 # For every individual that is not 0, change starting x point to mean of traps associated with encounters for that individual; leaves 0's there from the augmented population and also puts in activity center for augmented individuals that were randomly given an encounter history (caught at least 1 time)
-for(i in 1:nind) {
+for(i in 1:n_ind) {
   sst[i] <- mean( X[y[i, ] > 0] )
 }
 sst
@@ -111,36 +128,39 @@ sst
 # Calculate population size by summing all 1s associated with individuals within the population that were sampled (prob. of z = 1)
 # Calculate density by dividing pop. size by state space size (trapping length + buffers)
 
+if(!dir.exists("Code/JAGS")) dir.create("Code/JAGS", recursive = TRUE)
+
 cat ("
 model {
     alpha0 ~ dnorm(0, 0.1)
     logit(p0) <- alpha0
     alpha1 ~ dnorm(0, 0.1)
-    sigma <- sqrt(1/(2*alpha1))
+    sigma <- pow(1/(2*alpha1), 0.5) # circular home range?
     psi ~ dunif(0, 1)
     for(i in 1:M) {
         z[i] ~ dbern(psi)
         s[i] ~ dunif(xlimA[1], xlimA[2])
-        for(j in 1:8) {
-            d[i,j] <- sqrt(pow(s[i] - traplocsA[j], 2))
+        for(j in 1:n_traps) {
+            d[i,j] <- abs(s[i] - traplocsA[j])
             y[i,j] ~ dbin(p[i,j], K)
             p[i,j] <- z[i]*p0*exp(- alpha1 * d[i,j] * d[i,j])
       }
     }
 
-# error line 13, index out of range taking subset of y (y[i,j])
-
 N <- sum(z[])
-D <- N/300
+D <- N/(xlimA[2] - xlimA[1])
 }
-", file = "SCRA.txt")
+", file = "Code/JAGS/SCRA.txt")
 
-data <- list(y = EM, X=traplocsA, K=K, M=M, xlim=xlimA)
+jags_data <- list(y = y, traplocsA = traplocsA, K=K, M=M, xlimA=xlimA, n_traps = n_traps)
 inits <- function() {
-  list(alpha0=rnorm(1,-4,.4), alpha1=runif(1,1,2), s=sst, z=z)
+  list(alpha0=rnorm(1,-2,.4), alpha1=runif(1,1,2), s=sst, z=z)
 }
 
 parameters <- c("alpha0", "alpha1", "sigma", "N", "D")
 
-jinitA <- jags.model("SCRA.txt", data=data, inits=inits, n.chains = 3, n.adapt =1000)
+jinitA <- jagsUI(model.file = "Code/JAGS/SCRA.txt", parameters.to.save = parameters, data=jags_data, inits=inits, n.iter = 1000, n.chains = 3, n.adapt =500)
 # Get error parsing model file: syntax error online 11 near n
+
+library(R2jags)
+jinitA <- R2jags::jags(model.file = "Code/JAGS/SCRA.txt", parameters.to.save = parameters, data=jags_data, inits=inits, n.chains = 3, n.burnin = 500, n.iter = 1000)
